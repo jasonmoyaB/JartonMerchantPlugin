ccd85abb439ec0c514c954d3e410c808591d3bed
jason moya brenes <jason.moyabre.es@gmail.com>
2025-09-22 10:46:44 -0600

    fixed words

--- MiminecraftPlugins/src/main/java/PluginsJason/commands/ShopCommand.java
+++ MiminecraftPlugins/src/main/java/PluginsJason/commands/ShopCommand.java
@@ -105,9 +105,9 @@ public class ShopCommand implements CommandExecutor, Listener {
 
                 List<String> lore = itemMeta.getLore() != null ? new ArrayList<>(itemMeta.getLore()) : new ArrayList<>();
                 lore.add("");
-                lore.add("§6 §Ｂᴜʏ §f§l" + amount + " §7Ｆｏｒ §e$" + price);
+                lore.add("§6 § ʙᴜʏ §f§l" + amount + " §7ꜰᴏʀ §e$" + price);
                 lore.add("");
-                lore.add("§a§l✔ Ｃʟɪᴄᴋ ᴛᴏ ʙᴜʏ");
+                lore.add("§a§l✔ ᴄʟɪᴄᴋ ᴛᴏ ʙᴜʏ");
 
                 itemMeta.setLore(lore);
                 item.setItemMeta(itemMeta);
@@ -166,7 +166,7 @@ public class ShopCommand implements CommandExecutor, Listener {
         Economy econ = EconomyManager.getEconomy();
         if (econ == null || econ.getBalance(player) < price) {
             player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1f, 0.8f);
-            player.sendMessage("§cＩɴѕᴜꜰꜰɪᴄɪᴇɴᴛ ɢᴏʟᴅ!");
+            player.sendMessage("§cɪɴѕᴜꜰꜰɪᴄɪᴇɴᴛ ɢᴏʟᴅ!");
             return;
         }
 
@@ -185,7 +185,7 @@ public class ShopCommand implements CommandExecutor, Listener {
             player.getInventory().addItem(clickedItem.clone());
         }
 
-        player.sendMessage("§a✔ Ｐᴜʀᴄʜᴀѕᴇᴅ ᴀɴ ɪᴛᴇᴍ ꜰᴏʀ §e$" + price + " Ｇᴏʟᴅ ");
+        player.sendMessage("§a\uE110✔ ᴘᴜʀᴄʜᴀѕᴇᴅ ᴀɴᴅ ɪᴛᴇᴍ ꜰᴏʀ §e$" + price + " ɢᴏʟᴅ ");
         player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 1.2f);
     }
 
